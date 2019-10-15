715b93b225a5cc0e94cc7297eda62ee63f258f87
Bobby Carelse <bobby@byteorbit.com>
2019-10-15 10:20:22 +0200

    Section 28 done

--- tech_stack/src/components/LibraryList.js
+++ tech_stack/src/components/LibraryList.js
@@ -1,10 +1,32 @@
 import React, {Component} from 'react';
+import {FlatList} from 'react-native';
 import {connect} from 'react-redux';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
+
+    renderRow(library){
+        return (
+            <ListItem
+                library = {library}/>
+        );
+    }
+
     render(){
-        return;
+        return(
+            <FlatList
+                data={this.props.libraries}
+                keyExtractor={(library) => library.id}
+                renderItem={this.renderRow}/>
+        );
     }
 }
 
-export default connect()(LibraryList);
+//Takes our global state, and 
+//takes some properties and provides those 
+//properties to library list
+const mapStateToProps = state => {
+    return {libraries: state.libraries};
+};
+
+export default connect(mapStateToProps)(LibraryList);
