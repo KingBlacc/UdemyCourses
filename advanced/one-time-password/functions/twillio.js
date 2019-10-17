eb944301816acbbc6a94b1272bc526e394d2ca54
Bobby Carelse <bobby@byteorbit.com>
2019-10-17 15:12:43 +0200

    Removed firebase functions

--- advanced/one-time-password/functions/twillio.js
+++
@@ -1,6 +0,0 @@
-const twilio = require('twilio');
-
-const SID = 'ACcde59fdbc22e24c286e3544395642e2e';
-const token = '338c7866f73949cf21a2852826fc1516';
-
-module.exports = new twilio.Twilio(SID, token);
