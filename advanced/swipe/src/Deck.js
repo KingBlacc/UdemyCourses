b3f40de113ea04731c6b3454ae00d0ea6afdfb20
Bobby Carelse <bobby@byteorbit.com>
2019-10-17 10:25:27 +0200

    Swipe Complete

--- advanced/swipe/src/Deck.js
+++ advanced/swipe/src/Deck.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Animated, PanResponder, View, Dimensions} from 'react-native';
+import {Animated, PanResponder, View, Dimensions, LayoutAnimation, UIManager} from 'react-native';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
@@ -32,6 +32,17 @@ class Deck extends Component{
         });
 
         this.state = {panResponder, position, index: 0};
+    }
+
+    UNSAFE_componentWillReceiveProps(nextProps){
+        if(nextProps.data !== this.props.data){
+            this.setState({index: 0});
+        }
+    }
+
+    UNSAFE_componentWillUpdate(){
+        UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
+        LayoutAnimation.spring();
     }
 
     forceSwipe(direction){
@@ -83,22 +94,36 @@ class Deck extends Component{
                 return(
                     <Animated.View
                         key={item.id}
-                        style={this.getCardStyle()}
+                        style={[this.getCardStyle(), styles.cardStyle]}
                         {...this.state.panResponder.panHandlers}>
                             {this.props.renderCard(item)}
                     </Animated.View>
                 );
             }
 
-            return this.props.renderCard(item);
-        });
+            return (
+                <Animated.View 
+                    style={[styles.cardStyle, {top: 10 * (i - this.state.index)}]} 
+                    key={item.id}>
+                    {this.props.renderCard(item)}
+                </Animated.View>
+            );
+        }).reverse();
     }
 
     render(){
         return(
             <View>
                 {this.renderCards()}
-            </View>);
+            </View>
+        );
+    }
+}
+
+const styles = {
+    cardStyle: {
+        position: 'absolute',
+        width: SCREEN_WIDTH
     }
 }
 
